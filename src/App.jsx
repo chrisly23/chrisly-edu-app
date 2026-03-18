390e5d864b8eba9b40b501364798377256315ad5
louis laisina <luislais0811@gmail.com>
2026-03-18 15:46:26 +0900

    fix: menyamakan penulisan finalPayload

--- src/App.jsx
+++ src/App.jsx
@@ -1335,7 +1335,7 @@ const Generator = ({ type, user, appId, userData, usageCount, onSuccess, isDemo 
         const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(finalpayload)
+          body: JSON.stringify(finalPayload)
         });
         
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
